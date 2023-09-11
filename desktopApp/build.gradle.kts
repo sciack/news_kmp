c2a7f147380a788734aadea13e60d587d68dc6de
Mirko Sciachero <m.sciachero@gmail.com>
2023-09-11 14:10:07 +0800

    fix: implemented and review all the distribution

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -17,15 +17,27 @@ kotlin {
     }
 }
 
+val semVer: String by extra
+val rpmVersion: String by extra
+
+logger.warn("Version semantic $semVer")
+logger.warn("Version $version")
+
 compose.desktop {
     application {
         mainClass = "MainKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageVersion = version.toString().substringBefore('-')
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Rpm)
+            packageVersion =semVer.substringBefore("-")
+            licenseFile.set(File("../LICENSE.txt"))
             packageName = "News"
-
+            linux {
+                rpmPackageVersion = rpmVersion
+                rpmLicenseType = "Apache-2.0"
+                packageVersion =semVer.substringBefore("-")
+            }
         }
+
     }
 }
